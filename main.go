887be7deaf13dfe6a95806523a4815122f810a2c
kenny <0@kenny.cat>
2024-05-02 10:34:30 +0200

    remove file download function, change port to 8979, add prefix to temporary files, return JSON instead of text

--- main.go
+++ main.go
@@ -1,108 +1,86 @@
 package main
 
 import (
-	"crypto/rand"
-	"fmt"
-	"html/template"
-	"io/ioutil"
-	"log"
-	"mime"
-	"net/http"
-	"os"
-	"path/filepath"
+        "crypto/rand"
+        "fmt"
+        "html/template"
+        "io/ioutil"
+        "log"
+        "net/http"
+        "os"
+        "path/filepath"
 )
 
-const maxUploadSize = 2 * 1024 * 1024 // 2 mb
+const maxUploadSize = 20 * 1024 * 1024 // 20 MB
+
 var uploadPath = os.TempDir()
 
 func main() {
-	http.HandleFunc("/upload", uploadFileHandler())
+        http.HandleFunc("/upload", uploadFileHandler())
 
-	fs := http.FileServer(http.Dir(uploadPath))
-	http.Handle("/files/", http.StripPrefix("/files", fs))
-
-	log.Print("Server started on localhost:8080, use /upload for uploading files and /files/{fileName} for downloading")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+        log.Print("Server started on 0.0.0.0:8979, use /upload for uploading files")
+        log.Fatal(http.ListenAndServe(":8979", nil))
 }
 
 func uploadFileHandler() http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			t, _ := template.ParseFiles("upload.gtpl")
-			t.Execute(w, nil)
-			return
-		}
-		if err := r.ParseMultipartForm(maxUploadSize); err != nil {
-			fmt.Printf("Could not parse multipart form: %v\n", err)
-			renderError(w, "CANT_PARSE_FORM", http.StatusInternalServerError)
-			return
-		}
+        return func(w http.ResponseWriter, r *http.Request) {
+                if r.Method == "GET" {
+                        t, _ := template.ParseFiles("upload.gtpl")
+                        t.Execute(w, nil)
+                        return
+                }
 
-		// parse and validate file and post parameters
-		file, fileHeader, err := r.FormFile("uploadFile")
-		if err != nil {
-			renderError(w, "INVALID_FILE", http.StatusBadRequest)
-			return
-		}
-		defer file.Close()
-		// Get and print out file size
-		fileSize := fileHeader.Size
-		fmt.Printf("File size (bytes): %v\n", fileSize)
-		// validate file size
-		if fileSize > maxUploadSize {
-			renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
-			return
-		}
-		fileBytes, err := ioutil.ReadAll(file)
-		if err != nil {
-			renderError(w, "INVALID_FILE", http.StatusBadRequest)
-			return
-		}
+                if err := r.ParseMultipartForm(maxUploadSize); err != nil {
+                        renderError(w, "CANT_PARSE_FORM", http.StatusInternalServerError)
+                        return
+                }
 
-		// check file type, detectcontenttype only needs the first 512 bytes
-		detectedFileType := http.DetectContentType(fileBytes)
-		switch detectedFileType {
-		case "image/jpeg", "image/jpg":
-		case "image/gif", "image/png":
-		case "application/pdf":
-			break
-		default:
-			renderError(w, "INVALID_FILE_TYPE", http.StatusBadRequest)
-			return
-		}
-		fileName := randToken(12)
-		fileEndings, err := mime.ExtensionsByType(detectedFileType)
-		if err != nil {
-			renderError(w, "CANT_READ_FILE_TYPE", http.StatusInternalServerError)
-			return
-		}
+                file, fileHeader, err := r.FormFile("uploadFile")
+                if err != nil {
+                        renderError(w, "INVALID_FILE", http.StatusBadRequest)
+                        return
+                }
+                defer file.Close()
 
-		newFileName := fileName + fileEndings[0]
-		newPath := filepath.Join(uploadPath, newFileName)
-		fmt.Printf("FileType: %s, File: %s\n", detectedFileType, newPath)
+                fileSize := fileHeader.Size
+                if fileSize > maxUploadSize {
+                        renderError(w, "FILE_TOO_BIG", http.StatusBadRequest)
+                        return
+                }
 
-		// write file
-		newFile, err := os.Create(newPath)
-		if err != nil {
-			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
-			return
-		}
-		defer newFile.Close() // idempotent, okay to call twice
-		if _, err := newFile.Write(fileBytes); err != nil || newFile.Close() != nil {
-			renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
-			return
-		}
-		w.Write([]byte(fmt.Sprintf("SUCCESS - use /files/%v to access the file", newFileName)))
-	})
+                fileBytes, err := ioutil.ReadAll(file)
+                if err != nil {
+                        renderError(w, "INVALID_FILE", http.StatusBadRequest)
+                        return
+                }
+
+                fileName := randToken(12) + filepath.Ext(fileHeader.Filename)
+                newPath := filepath.Join(uploadPath, "httpup-"+fileName)
+
+                newFile, err := os.Create(newPath)
+                if err != nil {
+                        renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
+                        return
+                }
+                defer newFile.Close()
+
+                if _, err := newFile.Write(fileBytes); err != nil {
+                        renderError(w, "CANT_WRITE_FILE", http.StatusInternalServerError)
+                        return
+                }
+
+                w.Header().Set("Content-Type", "application/json")
+                w.Write([]byte(fmt.Sprintf(`{"status": "SUCCESS", "path": "%v"}`, newPath)))
+        }
 }
 
 func renderError(w http.ResponseWriter, message string, statusCode int) {
-	w.WriteHeader(statusCode)
-	w.Write([]byte(message))
+        w.WriteHeader(statusCode)
+        w.Write([]byte(fmt.Sprintf(`{"error": "%v"}`, message)))
 }
 
 func randToken(len int) string {
-	b := make([]byte, len)
-	rand.Read(b)
-	return fmt.Sprintf("%x", b)
+        b := make([]byte, len)
+        rand.Read(b)
+        return fmt.Sprintf("%x", b)
 }
